798b44faa16da6c09364328aaa34fc03a15c3e6a
Dastan <dastan.akynov.96@mail.ru>
2022-10-13 12:07:38 +0600

    Allow the cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(config.API);
 }
 bootstrap();
